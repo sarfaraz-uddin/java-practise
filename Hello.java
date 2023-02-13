8ee975fa7c54ce9675f301c75489e799700f7a07
sarfaraz-uddin <88956341+sarfaraz-uddin@users.noreply.github.com>
2023-02-13 10:30:04 +0545

    initial commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello{
+    public static void main(String[] args){
+        System.out.println("My name is sarfaraz uddin.");
+        System.out.println("I read in BIT.");
+        System.out.println("I play badminton.");
+        System.out.println("I am doing coding.");
+    }
+}
---
+++ Main.java
@@ -0,0 +1,10 @@
+//to run the java first javac Main.java, then java Main
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+        System.out.println("Hello world");
+
+    }
+}
