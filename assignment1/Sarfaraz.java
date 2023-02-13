3adf288c511e5c8ac02bbe9dd4f8392386cb65cb
sarfaraz-uddin <88956341+sarfaraz-uddin@users.noreply.github.com>
2023-02-13 17:15:59 +0545

    first assignment of java

---
+++ assignment1/Sarfaraz.java
@@ -0,0 +1,16 @@
+package assignment1;
+
+public class Sarfaraz {
+    public static void main(String[] args){
+        System.out.println("Name of the organization: Kamalpokhari Restaurant & Bar");
+        System.out.println("\n");
+        System.out.println("Address:");
+        System.out.println("---State: KM");
+        System.out.println("-----Zip Code: 44600");
+        System.out.println("----Country: Nepal");
+        System.out.println("\n");
+        System.out.println("Contact Information:");
+        System.out.println("----Mobile Number: 9865382489");
+        System.out.println("----Email: kamalpokhari.rnb@gmail.com");
+    }
+}
