107ad5bb9026ec37d49ab12b58f445841434b5e2
sarfaraz-uddin <88956341+sarfaraz-uddin@users.noreply.github.com>
2023-02-27 20:25:59 +0545

    missed classes of java

---
+++ bit_class_java_6th_4th_sem/src/StringLearning.java
@@ -0,0 +1,21 @@
+public class StringLearning {
+    public static void main(String[] args) {
+        // this will be created on the String pool
+        String name ="Roshan";
+        name = name + "Shrestha";
+        System.out.println(name);
+
+        // This will create the new Instance of the string
+        String name2 = new String("Hello world");
+
+        // Data overwrite on the heap memory.
+        StringBuffer name3 = new StringBuffer("Roshan");
+        name3.append("Shrestha");
+
+        System.out.println(name3);
+
+
+
+
+    }
+}
